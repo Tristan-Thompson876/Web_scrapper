198a00f355f6f6ceed73985f8dc03ab17e1bc240
Tristan-Thompson876 <tristanthomp.876@gmail.com>
2023-03-24 01:10:30 -0500

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+async function run() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://www.youtube.com/");
+    //await page.goto("https://www.traversymedia.com");
+
+    await page.screenshot({ path: "example.png", fullPage: true});
+    await page.pdf({path: "example.pdf", format: "A4"})
+
+    //const html = await page.content();
+    //console.log(html);
+
+    //const title = await page.evaluate(() => document.title);
+    //console.log(title);
+
+    //const text = await page.evaluate(() => document.body.innerText)
+
+    //const links = awaitpage.evaluate(()=> Array.from(document.querySelectorAll("a"), (e) => e.href));
+    //await browser.close();
+}
+run();
